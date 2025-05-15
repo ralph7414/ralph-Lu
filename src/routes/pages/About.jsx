8287e6411c1ae4bb227868a9c1ce78a3f112f001
ralph <ralphtesting88@gmail.com>
2025-05-15 16:36:51 +0800

    2025/05/15 ????

--- src/routes/pages/About.jsx
+++ src/routes/pages/About.jsx
@@ -10,11 +10,12 @@ const About = () => {
                     <div className="flex mx-auto max-xl:items-center justify-center max-2xl:flex-col max-xl:flex-row max-md:flex-col">
                         <div className="w-52 h-52 bg-sky-900 border-2 rounded-4xl m-auto">
                             <img
+                                className="w-full rounded-4xl"
                                 src="https://randomuser.me/api/portraits/men/1.jpg"
                                 alt=""
                             />
                         </div>
-                        <div className="mySelf flex flex-col justify-center items-start max-xl:items-center m-8 max-xl:m-auto max-md:m-8  ">
+                        <div className="mySelf flex flex-col justify-center items-start max-2xl:items-center m-8 max-xl:m-auto max-md:m-8  ">
                             <h1 className="text-4xl mb-2">羅仁甫</h1>
                             <h1 className="text-2xl mb-5">
                                 中原大學商業設計系
@@ -25,22 +26,47 @@ const About = () => {
                         </div>
                     </div>
 
-                    <div className="flex items-center justify-around my-10 mx-auto sticky top-[5rem]">
+                    <div className="flex items-center justify-around my-10 mx-auto sticky top-[5rem] max-lg:justify-center">
                         <Link>
-                            <button className="btn border-2 border-sky-500 bg-sky-900 p-4 rounded-2xl hover:bg-sky-950  ">
+                            <button className="btn border-2 border-sky-100 bg-sky-900 p-4 rounded-2xl hover:bg-sky-950 max-lg:m-10 max-md:m-2 ">
                                 CodePen
                             </button>
                         </Link>
                         <Link>
-                            <button className="btn border-2 border-sky-500 bg-sky-900 p-4 rounded-2xl hover:bg-sky-950 ">
+                            <button className="btn border-2 border-sky-100 bg-sky-900 p-4 rounded-2xl hover:bg-sky-950 max-lg:m-10 max-md:m-2">
                                 GitHub
                             </button>
                         </Link>
                         <Link>
-                            <button className="btn border-2 border-sky-500 bg-sky-900 p-4 rounded-2xl hover:bg-sky-950 ">
+                            <button className="btn border-2 border-sky-100 bg-sky-900 p-4 rounded-2xl hover:bg-sky-950 max-lg:m-10 max-md:m-2">
                                 Behance
                             </button>
                         </Link>
+                    </div>
+
+                    <div className="block">
+                        <h2>專業技能</h2>
+                        <h4>Skill</h4>
+                        <ul className="listSkill flex flex-wrap">
+                            <li className="w-1/3 p-5 flex flex-col text-center items-center mb-5">
+                                前端網頁<div className="circle"></div>
+                            </li>
+                            <li className="w-1/3 p-5 flex flex-col text-center items-center mb-5">
+                                後端資料<div className="circle"></div>
+                            </li>
+                            <li className="w-1/3 p-5 flex flex-col text-center items-center mb-5">
+                                程式開發<div className="circle"></div>
+                            </li>
+                            <li className="w-1/3 p-5 flex flex-col text-center items-center mb-5">
+                                平面設計<div className="circle"></div>
+                            </li>
+                            <li className="w-1/3 p-5 flex flex-col text-center items-center mb-5">
+                                UI/UX<div className="circle"></div>
+                            </li>
+                            <li className="w-1/3 p-5 flex flex-col text-center items-center mb-5">
+                                設計企劃<div className="circle"></div>
+                            </li>
+                        </ul>
                     </div>
                 </aside>
                 <article className="w-2/3 h-[2000px] max-xl:w-full">
