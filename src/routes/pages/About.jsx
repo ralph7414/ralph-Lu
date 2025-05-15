ed03ab5279ddd33801b521639c05c6c923106a7f
ralph <ralphtesting88@gmail.com>
2025-05-15 16:05:04 +0800

    2025/05/15 ???

--- src/routes/pages/About.jsx
+++ src/routes/pages/About.jsx
@@ -1,17 +1,51 @@
+import { Link } from 'react-router-dom'
+
 const About = () => {
     return (
         <>
             <header className="py-5 text-center text-4xl">關於我</header>
-            <section className="flex justify-center items-center">
-                <div className="w-52 h-52 bg-red-50 border-2">
-                    <img src="" alt="" />
-                </div>
-                <div className="mySelf">
-                    <h1>中原大學商業設計系</h1>
+            <nav></nav>
+            <section className="flex justify-center mx-40 max-xl:flex-col max-lg:mx-8">
+                <aside className=" w-1/3 max-xl:w-full ">
+                    <div className="flex mx-auto max-xl:items-center justify-center max-2xl:flex-col max-xl:flex-row max-md:flex-col">
+                        <div className="w-52 h-52 bg-sky-900 border-2 rounded-4xl m-auto">
+                            <img
+                                src="https://randomuser.me/api/portraits/men/1.jpg"
+                                alt=""
+                            />
+                        </div>
+                        <div className="mySelf flex flex-col justify-center items-start max-xl:items-center m-8 max-xl:m-auto max-md:m-8  ">
+                            <h1 className="text-4xl mb-2">羅仁甫</h1>
+                            <h1 className="text-2xl mb-5">
+                                中原大學商業設計系
+                            </h1>
 
-                    <h2>電話 0921457678</h2>
-                    <h2>信箱 ralphLu@gmail.com</h2>
-                </div>
+                            <h2 className="text-xl mb-2">電話 0921457678</h2>
+                            <h2 className="text-xl">信箱 ralphLu@gmail.com</h2>
+                        </div>
+                    </div>
+
+                    <div className="flex items-center justify-around my-10 mx-auto sticky top-[5rem]">
+                        <Link>
+                            <button className="btn border-2 border-sky-500 bg-sky-900 p-4 rounded-2xl hover:bg-sky-950  ">
+                                CodePen
+                            </button>
+                        </Link>
+                        <Link>
+                            <button className="btn border-2 border-sky-500 bg-sky-900 p-4 rounded-2xl hover:bg-sky-950 ">
+                                GitHub
+                            </button>
+                        </Link>
+                        <Link>
+                            <button className="btn border-2 border-sky-500 bg-sky-900 p-4 rounded-2xl hover:bg-sky-950 ">
+                                Behance
+                            </button>
+                        </Link>
+                    </div>
+                </aside>
+                <article className="w-2/3 h-[2000px] max-xl:w-full">
+                    222
+                </article>
             </section>
         </>
     )
--- src/routes/pages/Portfolio.jsx
+++ src/routes/pages/Portfolio.jsx
@@ -6,9 +6,9 @@ const Portfolio = () => {
             </h1>
             <header className=" text-center text-3xl mx-auto sticky top-[4rem]">
                 <ul className="flex justify-center bg-blue-900 text-blue-100 py-2">
-                    <li className="px-2">平面</li>
-                    <li className="px-2">網站</li>
-                    <li className="px-2">影片</li>
+                    <li className="px-2 mx-2">平面</li>
+                    <li className="px-2 mx-2">網站</li>
+                    <li className="px-2 mx-2">影片</li>
                 </ul>
             </header>
             <div className="h-lvh bg-blue-900"></div>
